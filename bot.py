1c9ab3855c1faf22e203a72d69c75e131df0db4c
Isaac Good <nuc-git@isaacgood.com>
2020-05-21 16:34:06 -0700

    Make the bot.py file actually work

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ import logging
 import os
 import sys
 
-from . import discussion_queue
-from . import inactive
-from . import prompts
-from . import quote_quiz
+import discussion_queue
+import inactive
+import prompts
+import quote_quiz
 from discord.ext import commands
 
 
@@ -17,9 +17,9 @@ Pruner = inactive.Pruner
 TalkQueue = discussion_queue.TalkQueue
 QuoteQuiz = quote_quiz.QuoteQuiz
 
-TOKEN = os.getenv('DISCORD_TOKEN')
 PROMPTS = {
   'creative-writing': 'prompts/prompts_writing.txt',
+  'testing': 'prompts/prompts_discuss.txt',
   'conversation-prompts': 'prompts/prompts_discuss.txt',
 }
 QUOTES = 'quote_quiz/quotes.txt'
@@ -31,11 +31,10 @@ def main():
   else:
     logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
 
+  dotenv.load_dotenv()
   token = os.getenv('DISCORD_TOKEN')
   if not token:
     raise RuntimeError('Missing DISCORD_TOKEN')
-
-  dotenv.load_dotenv()
 
   bot = commands.Bot(command_prefix='!')
   bot.add_cog(prompts.Prompts(PROMPTS))
